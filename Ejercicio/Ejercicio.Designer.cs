5f24248460c7c96a618b84fa9788720a8e3cb5f2
Hugo Mendez <hugo3011mendez@gmail.com>
2021-02-18 17:40:38 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio/Ejercicio.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Ejercicio
+{
+    partial class Ejercicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.etiquetaAviso1 = new Etiqueta_Aviso.EtiquetaAviso();
+            this.SuspendLayout();
+            // 
+            // etiquetaAviso1
+            // 
+            this.etiquetaAviso1.Color1Gradiente = System.Drawing.Color.Empty;
+            this.etiquetaAviso1.Color2Gradiente = System.Drawing.Color.Empty;
+            this.etiquetaAviso1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.etiquetaAviso1.Gradiente = false;
+            this.etiquetaAviso1.ImagenMarca = null;
+            this.etiquetaAviso1.Location = new System.Drawing.Point(106, 87);
+            this.etiquetaAviso1.Marca = Etiqueta_Aviso.eMarca.Nada;
+            this.etiquetaAviso1.Name = "etiquetaAviso1";
+            this.etiquetaAviso1.Size = new System.Drawing.Size(140, 23);
+            this.etiquetaAviso1.TabIndex = 0;
+            this.etiquetaAviso1.Text = "etiquetaAviso1";
+            // 
+            // Ejercicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 193);
+            this.Controls.Add(this.etiquetaAviso1);
+            this.Name = "Ejercicio";
+            this.Text = "Ejercicio 2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Etiqueta_Aviso.EtiquetaAviso etiquetaAviso1;
+    }
+}
+
---
+++ Etiqueta Aviso/EtiquetaAviso.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etiqueta_Aviso
+{
+    public enum eMarca // Defino el enumerado para los adornos
+    {
+        Nada,
+        Cruz,
+        Circulo,
+        ImagenDeForma
+    }
+
+
+    public partial class EtiquetaAviso : Control // Hago un componente personalizado, dibujando en vez de metiendo componentes de Windows Forms
+    {
+        public EtiquetaAviso()
+        {
+            InitializeComponent();
+        }
+
+        [Category("Marca")]
+        [Description("Indica que no se mostrará ninguna marca con la etiqueta")]
+        private eMarca marca = eMarca.Nada; // En esta variable usaremos todos los posibles datos elegidos del enumerado
+        // Aquí guardaremos el enumerado eMarca
+
+        public eMarca Marca
+        {
+            set
+            {
+                marca = value;
+                this.Refresh();
+            }
+            get { return marca; }
+        }
+
+        [Category("Marca")]
+        [Description("Guarda la imagen que se muestra como marca cuando se escoge esta opción")]
+        // Creo una variable para indicar la imagen que se dibujará si se escoge ImagenDeForma para la etiqueta aviso
+        private Image imagenMarca;
+        public Image ImagenMarca
+        {
+            set
+            {
+                imagenMarca = value;
+            }
+
+            get
+            {
+                return imagenMarca;
+            }
+        }
+
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+            int grosor = 0; //Grosor de las líneas de dibujo
+            int offsetX = 0; //Desplazamiento a la derecha del texto
+            int offsetY = 0; //Desplazamiento hacia abajo del texto
+
+            //Esta propiedad provoca mejoras en la apariencia o en la eficiencia a la hora de dibujar
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            //Dependiendo del valor de la propiedad marca dibujamos una
+            //Cruz o un Círculo
+            switch (Marca)
+            {
+                case eMarca.Circulo:
+                    grosor = 20;
+                    g.DrawEllipse(new Pen(Color.Green, grosor), grosor, grosor, this.Font.Height, this.Font.Height);
+
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor;
+                    break;
+
+
+                case eMarca.Cruz:
+                    grosor = 5;
+                    Pen lapiz = new Pen(Color.Red, grosor);
+
+                    g.DrawLine(lapiz, grosor, grosor, this.Font.Height, this.Font.Height);
+                    g.DrawLine(lapiz, this.Font.Height, grosor, grosor, this.Font.Height);
+
+                    offsetX = this.Font.Height + grosor;
+                    offsetY = grosor / 2;
+                    //Es recomendable liberar recursos de dibujo pues se pueden realizar muchos y cogen memoria
+
+                    lapiz.Dispose();
+                    break;
+
+
+                case eMarca.ImagenDeForma: // Dibujo la imagen establecida en la variable imagenMarca
+                    g.DrawImage(ImagenMarca, new Point(0, 0));
+
+                    // Y establezco los offsets para escribir el texto que se quiera
+                    offsetX = this.Font.Height + 10;
+                    offsetY = 30;
+                    break;
+            }
+
+            // Finalmente pintamos el Texto desplazado si fuera necesario
+            SolidBrush b = new SolidBrush(this.ForeColor);
+            g.DrawString(this.Text, this.Font, b, offsetX + grosor, offsetY);
+
+            Size tam = g.MeasureString(this.Text, this.Font).ToSize();
+            this.Size = new Size(tam.Width + offsetX + grosor, tam.Height + offsetY * 2);
+
+            b.Dispose();
+
+            Graphics ge = pe.Graphics; //Creo otro objeto graphics para enseñar las transformaciones
+
+            // Traslación
+            ge.TranslateTransform(100, 100);
+            ge.DrawLine(Pens.Red, 0, 0, 100, 0);
+            ge.ResetTransform();
+
+            // Rotación de 30o en sentido horario
+            ge.RotateTransform(30);
+            ge.DrawLine(Pens.Blue, 0, 0, 100, 0);
+            ge.ResetTransform();
+
+            // Traslación + rotación
+            ge.TranslateTransform(100, 100);
+            ge.RotateTransform(30);
+            ge.DrawLine(Pens.Green, 0, 0, 100, 0);
+            ge.ResetTransform();
+        }
+
+
+        protected override void OnTextChanged(EventArgs e) //Creamos un On para cada vez que se lanza el evento TextChanged
+        {
+            base.OnTextChanged(e);
+            this.Refresh(); // Y lanzo Refresh() para que se dibuje con todo
+        }
+
+        [Category("Fondo")]
+        [Description("Indica si se dibuja el gradiente de fondo o no")]
+        private bool gradiente = false;
+        public bool Gradiente
+        {
+            set
+            {
+                gradiente = value;
+            }
+
+            get
+            {
+                return gradiente;
+            }
+        }
+
+
+        [Category("Fondo")]
+        [Description("Es el primer color que influye en el gradiente al formarse")]
+        private Color color1Gradiente;
+        public Color Color1Gradiente
+        {
+            set
+            {
+                color1Gradiente = value;
+            }
+
+            get
+            {
+                return color1Gradiente;
+            }
+        }
+
+
+        [Category("Fondo")]
+        [Description("Es el segundo color que influye en el gradiente al formarse")]
+        private Color color2Gradiente;
+        public Color Color2Gradiente
+        {
+            set
+            {
+                color2Gradiente = value;
+            }
+
+            get
+            {
+                return color2Gradiente;
+            }
+        }
+
+
+        [Category("Eventos")]
+        [Description("Se lanza cuando se hace click sobre la marca, si existe")]
+        public event EventHandler ClickEnMarca;
+    }
+}
